784d97193d11b94a776f5ef3303cb6080baee4eb
Daniel <danieliozzi@gmail.com>
2014-11-20 05:41:16 +0200

    test cleanup , add sort of template for reference on when adding test

--- src/echo/echo.test.js
+++ src/echo/echo.test.js
@@ -1,37 +1,12 @@
-/* globals describe: false, before:false, after:false, afterEach: false*/
+/* globals describe: false */
 
-/** globals expect:true */
-//var expect = require('chai').expect;
-
-// var assert = require('assert'); //Mocha
-
-
-// var should = require('chai').should();
-var should = require('chai').should();
-/*should.exist
-should.not.exist
-should.equal
-should.not.equal
-should.Throw
-should.not.Throw*/
+require('chai').should();
 
 var echo = require('./echo');
 
 describe('Echo', function() {
-
-    before(function() {
-        // runs before all tests in this block
-    });
-    after(function() {
-        // runs after all tests in this block
-    });
-    beforeEach(function() {
-        // runs before each test in this block
-    });
-    afterEach(function() {
-        // runs after each test
-    });
-    it('Should Be Ok', function() {
+   
+    it('Should Echo things', function() {
         echo('hi').then(
             function(x) {
                 x.should.equal('hi');
---
+++ test/test.template.js
@@ -0,0 +1,38 @@
+/* globals describe: false, before:false, after:false, afterEach: false*/
+
+// globals expect:true  
+//var expect = require('chai').expect;
+
+// var assert = require('assert'); //from Mocha
+
+// var should = require('chai').should(); //To use should 
+// require('chai').should(); // to Attach Should to objects
+
+//Should Usage
+/*should.exist
+should.not.exist
+should.equal
+should.not.equal
+should.Throw
+should.not.Throw*/
+
+
+describe('Target', function() {
+
+    before(function() {
+        // runs before all tests in this block
+    });
+    after(function() {
+        // runs after all tests in this block
+    });
+    beforeEach(function() {
+        // runs before each test in this block
+    });
+    afterEach(function() {
+        // runs after each test
+    });
+    //Assertions
+    it('Should Be Ok', function() {
+        //{}.should.should.[what];
+    });
+});
